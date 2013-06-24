4015aea85de5cbcca2513b8411bcd956be151df7
iD Student <Student@idacademy-mac-2.princeton.edu>
2013-06-24 16:20:57 -0400

    Initial Commit

---
+++ Rock Paper Scissor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rock Paper Scissor
+//
+//  Created by iD Student on 6/24/13.
+//  Copyright (c) 2013 Tyler Maher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
